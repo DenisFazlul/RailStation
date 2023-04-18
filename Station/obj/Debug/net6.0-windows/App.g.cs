afd8b541b118859d10a662b8ac1b69d44b3abd33
fazlul.db <fazlul.db@progress.dsk1.ru>
2023-04-18 20:58:36 +0300

    Add Tests

--- Station/obj/Debug/net6.0-windows/App.g.cs
+++ Station/obj/Debug/net6.0-windows/App.g.cs
@@ -46,7 +46,7 @@ namespace Station {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
         public static void Main() {
             Station.App app = new Station.App();
             app.Run();
